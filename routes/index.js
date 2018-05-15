e80fb0fbf1c63e0fcf13abecfc0548a7ac802a0b
guokk <guokk@tsfa.com.cn>
2018-05-15 15:35:21 +0800

    404及错误页面处理

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,14 @@ module.exports = function (app) {
   app.use('/signout', require('./signout'))
   app.use('/posts', require('./posts'))
   app.use('/comments', require('./comments'))
+  app.use(function (req, res) {
+    if (!req.headersSend) {
+      res.status(404).render('404')
+    }
+  })
+  app.use(function(err, req, res, next) {
+    console.log(err)
+    req.flash('error', err.message)
+    res.redirect('/psots')
+  })
 }
